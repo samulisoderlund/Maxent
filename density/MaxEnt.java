6b2cf719bd1944f603337d9e2aa9aecd95cb51d7
Samuli Söderlund <samuli@unity3d.com>
2019-08-26 15:13:58 +0300

    Remove broken and obsolete version check

--- density/MaxEnt.java
+++ density/MaxEnt.java
@@ -7,10 +7,10 @@ a copy of this software and associated documentation files (the
 without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
-the following conditions: 
+the following conditions:
 
 The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software. 
+included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -18,7 +18,7 @@ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
 NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 package density;
@@ -29,17 +29,6 @@ import java.util.*;
 
 public class MaxEnt {
 
-    static void checkVersion() {
-	String version = System.getProperties().getProperty("java.version");
-	double v = Double.parseDouble(version.substring(0,3));
-	// can't call Utils, as Utils needs 1.4 to load (prefs.Preferences)
-	//	if (v < 1.4) Utils.fatalException("Java version is " + version + ", need 1.4 or later", null);
-	if (v < 1.5) {
-	    JOptionPane.showMessageDialog(null, "Java version is " + version + ", but Maxent needs 1.5 or later.", "Error", JOptionPane.ERROR_MESSAGE);
-	    System.exit(1);
-	}
-    }
-
     public static void main(String args[]) {
 	final Params params = new Params();
 	params.readFromArgs(args);
@@ -47,20 +36,18 @@ public class MaxEnt {
 	if (params.getboolean("visible")) {
 	    javax.swing.SwingUtilities.invokeLater(new Runnable() {
 		    public void run() {
-			checkVersion();
 			createAndShowGUI(params);
 		    }
 		});
 	}
 	else {
-	    checkVersion();
 	    params.setSelections();
 	    Runner runner = new Runner(params);
 	    runner.start();
 	    runner.end();
 	}
     }
-    
+
     private static void createAndShowGUI(Params params) {
         JFrame f = Utils.topLevelFrame = new JFrame("Maximum Entropy Species Distribution Modeling, Version " + Utils.version);
 	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
